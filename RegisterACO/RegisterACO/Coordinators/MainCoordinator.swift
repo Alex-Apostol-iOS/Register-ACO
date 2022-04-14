71e08b229cd0ef64177a85315d06759cebb53fee
Alex_Apostol <apostol516@gmail.com>
2022-04-14 15:39:26 +0200

    initialConfig

---
+++ RegisterACO/RegisterACO/Coordinators/MainCoordinator.swift
@@ -0,0 +1,53 @@
+//
+//  MainCoordinator.swift
+//  PruebaTecnica
+//
+//  Created by Alex Apostol on 4/4/22.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+
+    private var navigator: UINavigationController
+    
+    enum MainCoordinatorState {
+        case initial
+        case willShowLogin
+    }
+    
+    private var state: MainCoordinatorState
+    
+    init(with navigator: UINavigationController, state: MainCoordinatorState) {
+        self.navigator = navigator
+        self.state = state
+    }
+    
+    func start() {
+        self.loop()
+    }
+    
+    private func loop() {
+        self.state = next(self.state)
+        switch  self.state {
+        case .willShowLogin:
+            showLoginFlow()
+        case .initial:
+            fatalError("Unexpected Case in Main Coordinator")
+        }
+    }
+    
+    private func next(_ nextState: MainCoordinatorState) -> MainCoordinatorState {
+        switch nextState {
+        case .initial:
+            return .willShowLogin
+        case .willShowLogin:
+            return nextState
+        }
+    }
+    
+    private func showLoginFlow() {
+        
+    }
+    
+}
