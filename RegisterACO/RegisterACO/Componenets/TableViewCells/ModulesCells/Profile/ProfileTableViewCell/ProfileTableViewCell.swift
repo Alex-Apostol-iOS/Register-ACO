eb75c155f44d1df99efac7b145886d885463af4e
Alex_Apostol <apostol516@gmail.com>
2022-05-28 08:48:57 +0200

    origin/develop create profile table view cell

---
+++ RegisterACO/RegisterACO/Componenets/TableViewCells/ModulesCells/Profile/ProfileTableViewCell/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  RegisterACO
+//
+//  Created by Alex Apostol on 28/5/22.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func commoninit() {
+        
+    }
+}
