6c1369a652af950d62e109eaee1782e529a6b1cc
Alex_Apostol <apostol516@gmail.com>
2022-05-28 09:24:46 +0200

    origin/develop create RegisterUserInfoHeader

---
+++ RegisterACO/RegisterACO/Componenets/ReusableView/RegisterUserInfoHeader/RegisterUserInfoHeader.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterUserInfoHeader.swift
+//  RegisterACO
+//
+//  Created by Alex Apostol on 28/5/22.
+//
+
+import UIKit
+
+class RegisterUserInfoHeader: UIStackView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+}
