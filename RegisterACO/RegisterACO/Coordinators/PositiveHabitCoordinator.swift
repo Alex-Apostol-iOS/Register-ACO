c058e1acb3e3ce99dbe5185dd6736a41580fa2b9
Alex_Apostol <apostol516@gmail.com>
2022-07-02 11:14:01 +0200

    origin/develop Create positive habit coordinator

---
+++ RegisterACO/RegisterACO/Coordinators/PositiveHabitCoordinator.swift
@@ -0,0 +1,52 @@
+//
+//  PositiveHabitCoordinator.swift
+//  RegisterACO
+//
+//  Created by Alex Apostol on 2/7/22.
+//
+
+import UIKit
+
+class PositiveHabitCoordinator: Coordinator {
+    
+     enum PositiveHabitCoordinatorState {
+        case initial
+        case willShowPositiveHabitInfoView
+    }
+    
+    private let navigator: UINavigationController
+    private var state: PositiveHabitCoordinatorState
+    
+    
+    init(on navigator: UINavigationController, with state: PositiveHabitCoordinatorState = .initial) {
+        self.navigator = navigator
+        self.state = state
+    }
+    
+    func start() {
+        state = .initial
+    }
+    
+    private func loop() {
+        
+        switch next(nextState: state) {
+        case .willShowPositiveHabitInfoView:
+            break
+        case .initial:
+            fatalError("no implementation for \(next(nextState: state)) on PositiveHabitCoordinator")
+        }
+    }
+    
+    private func next(nextState: PositiveHabitCoordinatorState) -> PositiveHabitCoordinatorState {
+        switch nextState {
+        case .initial:
+            return .willShowPositiveHabitInfoView
+        default: return nextState
+        }
+    }
+    
+    private func willShowPositiveHabitInfoView() {
+        
+    }
+    
+}
