54e67af7fdb1749a2c1fff5b34d9579ff5344d87
Alex_Apostol <apostol516@gmail.com>
2022-07-08 18:35:40 +0200

    origin/develop add positive habit info view scroll

--- RegisterACO/RegisterACO/Modules/PositiveHabit/PositiveHabitInfoView/View/PostiveHabitInfoViewViewController.swift
+++ RegisterACO/RegisterACO/Modules/PositiveHabit/PositiveHabitInfoView/View/PostiveHabitInfoViewViewController.swift
@@ -106,6 +106,22 @@ class PostiveHabitInfoViewViewController: RegisterAcoNavigationController {
         stackView.axis = .vertical
         return stackView
     }()
+    
+    private lazy var scrollView: UIScrollView = {
+       let scroll = UIScrollView()
+        scroll.backgroundColor = .clear
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        scroll.showsVerticalScrollIndicator = false
+        scroll.backgroundColor = UIColor.white
+        return scroll
+    }()
+    
+    private lazy var customContentView: UIView = {
+       let content = UIView()
+        content.backgroundColor = UIColor.white
+        content.translatesAutoresizingMaskIntoConstraints = false
+        return content
+    }()
         
     init (presenter: PostiveHabitInfoViewPresenterProtocol) {
         self.presenter = presenter
@@ -119,16 +135,41 @@ class PostiveHabitInfoViewViewController: RegisterAcoNavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpStackViewLayout()
+        setUpScrollViewLayout()
+        setUpContentViewLayout()
+        setUpMainStackViewLayout()
     }
     
-    private func setUpStackViewLayout() {
-        view.addSubview(mainStackView)
+    private func setUpMainStackViewLayout() {
+        self.customContentView.addSubview(mainStackView)
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
-        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
-        mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+       
+        mainStackView.topAnchor.constraint(equalTo: self.customContentView.topAnchor, constant: 16).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo: self.customContentView.leadingAnchor, constant: 16).isActive = true
+        mainStackView.trailingAnchor.constraint(equalTo: self.customContentView.trailingAnchor, constant: -16).isActive = true
+        let constraint = self.mainStackView.bottomAnchor.constraint(greaterThanOrEqualTo: self.customContentView.bottomAnchor, constant: -80)
+        constraint.priority = UILayoutPriority(250)
+        constraint.isActive = true
+    }
+    
+    private func setUpScrollViewLayout() {
+        self.view.addSubview(scrollView)
+        scrollView.topAnchor.constraint(equalTo:  self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo:  self.view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo:  self.view.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo:  self.view.bottomAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo:  self.view.widthAnchor).isActive = true
+        scrollView.centerXAnchor.constraint(equalTo:  self.view.centerXAnchor).isActive = true
+    }
+    
+    private func setUpContentViewLayout() {
+        scrollView.addSubview(customContentView)
+        customContentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        customContentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        customContentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        customContentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -80).isActive = true
+        customContentView.widthAnchor.constraint(equalTo:    self.view.widthAnchor).isActive = true
+        customContentView.heightAnchor.constraint(greaterThanOrEqualTo:  self.view.heightAnchor).isActive = true
     }
     
     @objc
